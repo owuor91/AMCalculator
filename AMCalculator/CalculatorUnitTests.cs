c6a5d6978080562a7a8559c36b3b43ec7b1e9400
owuor91 <owuor91@gmail.com>
2015-07-13 16:24:51 +0300

    create unit test for initial actor answer state

--- AMCalculator/CalculatorUnitTests.cs
+++ AMCalculator/CalculatorUnitTests.cs
@@ -6,5 +6,12 @@ namespace AMCalculator.Tests
 {
     public class CalculatorUnitTests : TestKit
     {
+        [Fact]
+        public void Answer_should_initially_be_0()
+        {
+            var calculatorRef = ActorOfAsTestActorRef<CalculatorActor>("calculator");
+            var calculator = calculatorRef.UnderlyingActor;
+            Assert.Equal(0, calculator.Answer);
+        }
     }
 }
