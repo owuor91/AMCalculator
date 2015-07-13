55281e40651efd7412b4261bf7d3ae35a622b1d1
owuor91 <owuor91@gmail.com>
2015-07-13 16:40:50 +0300

    addition test created

--- AMCalculator/CalculatorUnitTests.cs
+++ AMCalculator/CalculatorUnitTests.cs
@@ -13,5 +13,13 @@ namespace AMCalculator.Tests
             var calculator = calculatorRef.UnderlyingActor;
             Assert.Equal(0, calculator.Answer);
         }
+
+        public void add_1_and_6_to_give_7()
+        {
+            TestActorRef<CalculatorActor> calculatorRef = ActorOfAsTestActorRef<CalculatorActor>("calculator");
+            calculatorRef.Tell(new Add(1, 7));
+            CalculatorActor calculator = calculatorRef.UnderlyingActor;
+            Assert.Equal(7, calculator.Answer);
+        }
     }
 }
